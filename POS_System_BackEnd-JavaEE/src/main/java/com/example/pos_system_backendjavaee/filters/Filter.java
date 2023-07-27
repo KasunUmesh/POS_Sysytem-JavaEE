3b127f16ab2b878e052f78994d74431e83545bd1
KasunUmesh <kasunumesh38@gmail.com>
2023-07-28 00:31:56 +0530

    Added info to the Filter Class

---
+++ POS_System_BackEnd-JavaEE/src/main/java/com/example/pos_system_backendjavaee/filters/Filter.java
@@ -0,0 +1,24 @@
+package com.example.pos_system_backendjavaee.filters;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter
+public class Filter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String origin = req.getHeader("Origin");
+        if (origin.contains("http://")) {
+            res.setHeader("Access-Control-Allow-Origin", origin);
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEADER");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            res.setHeader("Access-Control-Expose-Headers","Content-Type");
+        }
+        chain.doFilter(req, res);
+    }
+}
